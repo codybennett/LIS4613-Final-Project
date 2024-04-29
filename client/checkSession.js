f4651995e9443f3841fd70cda06d18ddbbb1d72c
Cody Bennett <cbennett0926@gmail.com>
2024-04-29 15:24:11 -0500

    Style changes

--- client/checkSession.js
+++ client/checkSession.js
@@ -6,12 +6,13 @@ fetch(`http://localhost:3000/check-session`, {
 }).then(response => {
     if (response.ok) {
         document.getElementById("auth-login-button").style.display = "none"
-        document.getElementById("auth-logout-button").style.display = "inline"
+        document.getElementById("auth-logout-button").className = "auth-button"
         document.getElementById("login-menu").style.display = "none"
     } else {
         document.getElementById("auth-logout-button").style.display = "none"
         document.getElementById("add-item-bttn").style.display = "none";
         document.getElementById("remove-item-bttn").style.display = "none";
+        document.getElementById("update-item-bttn").style.display = "none";
         document.getElementById("remove-selected-item-bttn").style.display = "none";
         document.getElementById("update-selected-item-bttn").style.display = "none";
 
